c59a8810a6b29d145c1a046e05ac66b39f555a27
hienpetshop <locpetshop@gmail.com>
2025-08-26 23:27:53 +0700

    Create pinokio.js

---
+++ pinokio.js
@@ -0,0 +1,16 @@
+module.exports = {
+  id: "hunyuan3d-2.1",
+  meta: {
+    name: "Hunyuan3D-2.1",
+    version: "1.0.0",
+    author: "hienpetshop",
+    description: "Run Hunyuan3D-2.1 with Gradio UI"
+  },
+  // các nút ở sidebar
+  pages: [
+    { name: "Install", path: "install.js" },
+    { name: "Start",   path: "start.js"   },
+    { name: "Reset",   path: "reset.js"   },
+    { name: "Update",  path: "update.js"  }
+  ]
+};
