389ca8a3cb59bc83420157bc8f383221c45b81a5
hienpetshop <locpetshop@gmail.com>
2025-08-27 00:00:11 +0700

    Update start.js

--- start.js
+++ start.js
@@ -1,13 +1,12 @@
-module.exports = {
-  // bạn có thể đổi port khác nếu muốn
+module.exports = async () => ({
+  persist: true,
+  shell: "cmd",
   env: {
     GRADIO_SERVER_NAME: "127.0.0.1",
     GRADIO_SERVER_PORT: "42003"
   },
   ports: [42003],
-  persist: true, // mở Terminal theo dõi log
   run: [
-    // chạy Gradio
     "python gradio_app.py --host 127.0.0.1 --port 42003"
   ]
-};
+});
