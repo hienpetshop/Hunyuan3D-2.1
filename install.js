337ee23283b0bcd5676d106696bffc50307cbd58
hienpetshop <locpetshop@gmail.com>
2025-08-27 00:10:40 +0700

    Update install.js

--- install.js
+++ install.js
@@ -1,10 +1,15 @@
-module.exports = async () => ({
+module.exports = {
   persist: true,
   shell: "cmd",
   run: [
-    "python --version",
-    "where python",
+    "echo === STEP1: SHELL OK ===",
+    "ver",
+    "echo === STEP2: PYTHON PATH ===",
+    "where python || echo NO_PYTHON",
+    "echo === STEP3: PY VERSION ===",
+    "python -V || echo PY_ERROR",
+    "echo === STEP4: PIP INSTALL ===",
     "python -m pip install --upgrade pip",
     "python -m pip install -r requirements.txt"
   ]
-});
+};
