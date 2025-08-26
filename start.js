603332347c7d15484a3f51afcb2db26de6541a75
hienpetshop <locpetshop@gmail.com>
2025-08-27 00:56:19 +0700

    Update start.js

--- start.js
+++ start.js
@@ -1,15 +1,18 @@
 module.exports = {
-  path: ".",
-  persist: true,
-  shell: "cmd",
-  env: {
-    GRADIO_SERVER_NAME: "127.0.0.1",
-    GRADIO_SERVER_PORT: "42003"
-  },
-  ports: [42003],
   run: [
-    // bắt buộc gọi conda_hook trước khi chạy python
-    "conda_hook && conda deactivate && conda deactivate && conda activate base",
-    "python gradio_app.py --host 127.0.0.1 --port 42003"
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: ".",
+        env: {
+          GRADIO_SERVER_NAME: "127.0.0.1",
+          GRADIO_SERVER_PORT: "42003"
+        },
+        message: [
+          "python gradio_app.py --host 127.0.0.1 --port 42003"
+        ]
+      }
+    }
   ]
-};
+}
